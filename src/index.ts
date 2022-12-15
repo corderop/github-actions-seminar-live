615c64137b835df4b626d5ea72065c43f6985de5
Pablo Cordero <pablocorderoromero@gmail.com>
2022-12-15 11:49:29 +0100

    testing 3

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,31 @@
 import * as core from "@actions/core"
+import * as github from "@actions/github"
 
-async function run() {
+function getInputs() {
   core.startGroup("Getting inputs")
   const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN", { required: true })
   const RAPID_API_KEY = core.getInput("RAPID_API_KEY", { required: true })
   core.info("Inputs retrieved properly.")
   core.endGroup()
 
-  console.log(GITHUB_TOKEN, RAPID_API_KEY)
+  return { GITHUB_TOKEN, RAPID_API_KEY }
+}
+
+async function run() {
+  const { GITHUB_TOKEN, RAPID_API_KEY } = getInputs()
+
+  const context = github.context
+  const { pull_request: pullRequest } = context.payload
+  const comment = pullRequest?.body
+  const number = pullRequest?.number
+
+  console.log(pullRequest)
+
+  if (!comment || !number) {
+    core.setFailed("No comment or pull request number found.")
+  }
+
+  console.log(comment, number)
 }
 
 run()
