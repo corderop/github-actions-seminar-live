10926fa3cbf3412d67a7185ac3f2979d984a4bfb
Pablo Cordero <pablocorderoromero@gmail.com>
2022-12-15 12:01:00 +0100

    testing 4

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import * as core from "@actions/core"
 import * as github from "@actions/github"
+import fetch from "node-fetch"
 
 function getInputs() {
   core.startGroup("Getting inputs")
@@ -11,21 +12,62 @@ function getInputs() {
   return { GITHUB_TOKEN, RAPID_API_KEY }
 }
 
-async function run() {
-  const { GITHUB_TOKEN, RAPID_API_KEY } = getInputs()
-
+function getMessageAndPrNumber() {
   const context = github.context
   const { pull_request: pullRequest } = context.payload
   const comment = pullRequest?.body
   const number = pullRequest?.number
 
-  console.log(pullRequest)
+  return { comment, number }
+}
+
+interface Translation {
+  translatedText: string
+  detectedSourceLanguage: string
+}
+
+interface ReponseBody {
+  data: {
+    translations?: Translation[]
+  }
+}
+
+async function run() {
+  const { GITHUB_TOKEN, RAPID_API_KEY } = getInputs()
+  const { comment, number } = getMessageAndPrNumber()
 
   if (!comment || !number) {
     core.setFailed("No comment or pull request number found.")
+    return
   }
 
-  console.log(comment, number)
+  const encodedParams = new URLSearchParams()
+  encodedParams.append("target", "en")
+  encodedParams.append("q", comment)
+
+  const url = "https://google-translate1.p.rapidapi.com/language/translate/v2"
+
+  const options = {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded",
+      "Accept-Encoding": "application/gzip",
+      "X-RapidAPI-Key": RAPID_API_KEY,
+      "X-RapidAPI-Host": "google-translate1.p.rapidapi.com",
+    },
+    body: encodedParams,
+  }
+
+  const response = await fetch(url, options)
+  const body = (await response.json()) as ReponseBody
+  const translatedText = body?.data?.translations?.[0].translatedText
+
+  if (!translatedText) {
+    core.setFailed("No translated text found.")
+    return
+  }
+
+  console.log(translatedText)
 }
 
 run()
