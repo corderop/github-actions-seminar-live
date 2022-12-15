8cb31b1546000e4fb86cfd8ba0dbbeba84d454a9
Pablo Cordero <pablocorderoromero@gmail.com>
2022-12-15 11:39:38 +0100

    testing 2

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,13 @@
+import * as core from "@actions/core"
+
 async function run() {
-  console.log("Hello, world!")
+  core.startGroup("Getting inputs")
+  const GITHUB_TOKEN = core.getInput("GITHUB_TOKEN", { required: true })
+  const RAPID_API_KEY = core.getInput("RAPID_API_KEY", { required: true })
+  core.info("Inputs retrieved properly.")
+  core.endGroup()
+
+  console.log(GITHUB_TOKEN, RAPID_API_KEY)
 }
 
 run()
